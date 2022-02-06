7a968e83d71876337c5e5aeeaf4d9d1b838d1626
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-02-06 17:18:13 -0600

    fixed issue

--- CaseStudy2/src/RecruitmentProcess.java
+++ CaseStudy2/src/RecruitmentProcess.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class RecruitmentProcess {
+import java.util.Scanner;
+
+public class RecruitmentProcess {
+    static String RecruitmentProcess;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Recruitment Process"+"\n"+"1.ExternalRp"+"\n"+"2.EmployeeRp"); int Process = sc.nextInt();
+        if (Process== 1) {
+            Recruitment rp = new ExternalRp();
+            RecruitmentProcess="External RecruitmentProcess ";
+            rp.procedure();
+        }
+        else if (Process == 2) {
+            Recruitment rp = new EmployeeRp();
+            RecruitmentProcess="Employee Rreferal Process";
+            rp.procedure();
+        } }
 }
