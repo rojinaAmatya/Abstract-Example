a8ad5586f31d893c8849e46655bd04348101e4e8
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-02-07 13:05:13 -0600

    added wiring class

---
+++ CaseStudy3/src/Wiring.java
@@ -0,0 +1,17 @@
+public class Wiring { //using encapsulation
+    private String wiringType;
+
+    public String wiringType(){
+        return wiringType;
+    }
+
+    public String getWiringType() {
+        return wiringType;
+    }
+
+    public void setWiringType (String wiringType)
+    {
+        this.wiringType = wiringType;
+    }
+
+}
